75dd1dfdec565bc930288eb4320debcef2bb1fd3
Alan Cushway <acushway@hotmail.com>
2024-11-13 15:37:26 +1100

    version 3.0.7 - update swift package file

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "PINCache",
+    name: "HHPPINCache",
     platforms: [
         .macOS(.v10_10),
         .iOS(.v9),
@@ -14,20 +14,20 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PINCache",
-            targets: ["PINCache"]),
+            name: "HHPPINCache",
+            targets: ["HHPPINCache"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pinterest/PINOperation.git", from: "1.2.0"),
+        .package(url: "https://github.com/acushway/PINOperation.git", from: "1.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PINCache",
-            dependencies: ["PINOperation"],
+            name: "HHPPINCache",
+            dependencies: ["HHPPINOperation"],
             path: "Source",
             exclude: ["Info.plist"],
             publicHeadersPath: ".",
@@ -35,8 +35,8 @@ let package = Package(
                 .define("NS_BLOCK_ASSERTIONS", to: "1", .when(configuration: .release)),
             ]),
         .testTarget(
-            name: "PINCacheTests",
-            dependencies: ["PINCache"],
+            name: "HHPPINCacheTests",
+            dependencies: ["HHPPINCache"],
             path: "Tests",
             exclude: ["Info.plist"],
             resources: [.process("Default-568h@2x.png")],
